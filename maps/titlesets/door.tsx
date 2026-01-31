07a9f53be9e3e40712b2c01382a2b1b474a8ae8a
athira1221 <athiraaravind1221@gmail.com>
2026-01-31 06:54:53 +0530

    assests added

---
+++ maps/titlesets/door.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.11.2" name="door" tilewidth="32" tileheight="32" tilecount="30" columns="5">
+ <image source="assests/titlesets/craftpix-net-169442-free-2d-top-down-pixel-dungeon-asset-pack/PNG/doors_lever_chest_animation.png" trans="ff00ff" width="160" height="192"/>
+</tileset>
